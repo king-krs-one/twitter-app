eaca27a2985fc88c63741aa5dbf0fb8c8131fd68
king-krs-one <jonathan.nickels@googlemail.com>
2023-08-20 10:43:50 +0200

    add auth user model

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -160,3 +160,5 @@ DJOSER = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.UserAccount"
